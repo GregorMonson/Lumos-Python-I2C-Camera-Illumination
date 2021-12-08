f7c5c9ffd26f5db52b6c82e998b0fb76516146da
GregorMonson <gregor.monson@lumiradx.com>
2021-12-08 10:45:22 +0000

    Create lumos.py

---
+++ lumos.py
@@ -0,0 +1,28 @@
+import smbus
+
+#Camera bus id
+CAMERA_BUS = 1
+#Camera I2C address
+CAMERA_ADDR = 0x00
+
+class Camera():
+    def __init__(self, address, camera_bus):
+        self.addr = address    
+        self.bus = smbus.SMBus(camera_bus)
+
+        return True
+
+    def write(self, register ,command):
+        self.bus.write_byte_data(self.addr, register, command)
+
+        return True
+
+    def read(self, register):
+        self.bus.read_byte_data(self.addr, register)
+
+        return True
+
+camera = Camera(CAMERA_ADDR, CAMERA_BUS)
+
+camera.write(0x00, 0x01)
+
